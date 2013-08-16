2765b3d3687991f20483ae6b07a7661297225f22
Lucas <metallucas.ds@gmail.com>
2013-08-15 23:03:15 -0300

    history api

---
+++ history-api/js/main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  'use strict';
+
+  var el = document.getElementById('foo');
+
+  el.onclick = function(e) {
+    alert('text');
+  };
+
+  // var stateObj = { foo: "bar" };
+  // history.pushState(stateObj, "page 2", "bar.html");
+
+
+}
