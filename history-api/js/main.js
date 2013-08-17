691dfe9b85ac7c5a6e29d122b7d42fb2808edd14
Lucas <metallucas.ds@gmail.com>
2013-08-16 23:27:12 -0300

    Update main.js

--- history-api/js/main.js
+++ history-api/js/main.js
@@ -2,9 +2,9 @@ window.onload = function() {
   'use strict';
 
   var el = document.getElementById('foo');
-
+  var currentState = history.state;
   el.onclick = function(e) {
-    alert('text');
+    alert(currentState);
   };
 
   // var stateObj = { foo: "bar" };
